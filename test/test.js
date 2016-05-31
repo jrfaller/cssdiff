927cecbec1dda960640df0050b7b25b5291cccb9
Jean-Rémy Falleri <jr.falleri@gmail.com>
2016-05-31 21:06:52 +0200

    added test for cssdiff.

--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
 require('shelljs/global');
 
 var assert = require('chai').assert;
+
 describe('cssdump', function() {
 	it('should extract css information from a website', function () {
 		this.timeout(10000);
@@ -23,5 +24,15 @@ describe('cssdump', function() {
 		var body = res['site']['HTML > BODY:nth-child(2)'];
 		assert.property(body, 'margin-top');
 		assert.propertyVal(body, 'margin-top', '10px');
+	});
+});
+
+describe('cssdiff', function() {
+	it('should diff two css dumps', function () {
+		this.timeout(10000);
+		var stdout = exec('node cssdiff.js test/v0.json test/v1.json', {silent: true}).stdout;
+		var res = JSON.parse(stdout);
+		var expected = JSON.parse('{"site":{"html":[{"action":"changed","property":"border","value":"10px"},{"action":"removed","property":"font-size","value":"10pt"}],"ul":[{"action":"changed","property":"border","value":"2px"},{"action":"added","property":"color","value":"black"}]}}');
+		assert.deepEqual(res, expected);
 	});
 });
